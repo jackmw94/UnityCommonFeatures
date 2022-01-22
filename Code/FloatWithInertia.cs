08a3c54b9d90fbc2bc2fb4d3d6faaefad6bd057d
Jack Walker <jackmw94@gmail.com>
2022-01-22 19:53:15 +0000

    Float with inertia

---
+++ Code/FloatWithInertia.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityCommonFeatures
+{
+    [Serializable]
+    public class FloatWithInertia
+    {
+        [SerializeField] private float _inertia = 0.98f;
+        [SerializeField] private float _damping = 0.11f;
+
+        private float _target = 0f;
+        private float _current = 0f;
+        private float _previousChange = 0f;
+
+        public float Value => _current;
+        
+        public void SetTargetValue(float target)
+        {
+            _target = target;
+        }
+
+        public void Tick()
+        {
+            _current = GetInertiaChange(_inertia, _current, _target, _previousChange, out _previousChange);
+        }
+        
+        private float GetInertiaChange(float inertia, float current, float target, float previousChange, out float change)
+        {
+            change = inertia * previousChange + (target - current) * (1f - inertia);
+            float next = current + change;
+            return Mathf.Lerp(next, target, _damping);
+        }
+
+        public void SetInertia(float inertia)
+        {
+            _inertia = inertia;
+        }
+
+        public void SetDamping(float damping)
+        {
+            _damping = damping;
+        }
+        
+        public override string ToString()
+        {
+            return $"Current = {_current}, Target = {_target}";
+        }
+
+        public void CopyFrom(FloatWithInertia other)
+        {
+            _inertia = other._inertia;
+            _damping = other._damping;
+        }
+        
+        public void SetCurrentValueInstant(float currentValue)
+        {
+            _current = currentValue;
+        }
+    }
+}
