484478e2ecd424d676a2e0d0bbcea8980613c196
Jack Walker <jackmw94@gmail.com>
2022-02-20 17:18:04 +0000

    Wrapper class for simple button listener classes

---
+++ Code/UI/ButtonBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityCommonFeatures
+{
+    /// <summary>
+    /// Useful to remove some of the repetitive code involved with setting up a class
+    /// whose primary purpose is to execute some function on a button click
+    /// </summary>
+    [RequireComponent(typeof(Button))]
+    public abstract class ButtonBehaviour : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+
+        [Conditional("UNITY_EDITOR")]
+        protected virtual void OnValidate()
+        {
+            if (!_button)
+            {
+                _button = GetComponent<Button>();
+            }
+        }
+
+        protected virtual void OnEnable()
+        {
+            _button.onClick.AddListener(OnButtonClicked);
+        }
+
+        protected virtual void OnDisable()
+        {
+            _button.onClick.RemoveListener(OnButtonClicked);
+        }
+        
+        protected abstract void OnButtonClicked();
+    }
+}
