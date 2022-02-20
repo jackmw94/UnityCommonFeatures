2a108d5f822abf4449013ad14b61153800591105
Jack Walker <jackmw94@gmail.com>
2022-02-20 17:18:14 +0000

    Momentary button

---
+++ Code/UI/MomentaryButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace UnityCommonFeatures
+{
+    public class MomentaryButton : Button
+    {
+        public bool IsHeld { get; private set; }
+
+        public override void OnPointerDown(PointerEventData eventData)
+        {
+            base.OnPointerDown(eventData);
+            IsHeld = true;
+        }
+
+        public override void OnPointerUp(PointerEventData eventData)
+        {
+            base.OnPointerUp(eventData);
+            IsHeld = false;
+        }
+    }
+}
